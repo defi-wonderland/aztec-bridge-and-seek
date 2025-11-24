86a6c4742e392c7e4b0fb01fcb39b3fd838466dd
shaito <shaito@defi.sucks>
2025-11-24 17:21:51 -0300

    chore: remove sidebar

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -5,5 +5,4 @@ export { TokenBalanceCard } from './TokenBalanceCard';
 export { Layout } from './Layout';
 export { SettingsCard } from './SettingsCard';
 export { SendersCard } from './SendersCard';
-export { Sidebar } from './Sidebar';
 export { MainContent } from './MainContent';
